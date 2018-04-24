cb93733e1e375390bfa931b0731e444c7ad44557
Wing Kei Leung <winky.leung@gmail.com>
2018-04-23 23:46:32 -0700

    Adding Apollo-engine for monitoring query and time

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
+import compression from 'compression';
+import { ApolloEngine } from 'apollo-engine';
+
 import schema from './data/schema';
 
 const GRAPHQL_PORT = 3000;
+const ENGINE_API_KEY = 'api_key_here'; 
 
 const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+const engine = new ApolloEngine({
+  apiKey: ENGINE_API_KEY,
+});
+
+graphQLServer.use(compression());
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema, tracing: true }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-graphQLServer.listen(GRAPHQL_PORT, () =>
-  console.log(
+engine.listen({ port: GRAPHQL_PORT, expressApp: graphQLServer},
+  () => console.log(
     `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
   )
 );
