f5b7124c3b20c639168f73963207bb582a7ceafc
Wing Kei Leung <winky.leung@gmail.com>
2018-04-23 23:10:01 -0700

    Finished hooking up to mysql, and using casual to mock seed for db

---
+++ data/connectors.js
@@ -0,0 +1,51 @@
+import Sequelize from 'sequelize';
+import casual from 'casual';
+import _ from 'lodash';
+import fetch from 'node-fetch';
+
+const db = new Sequelize('blog', 'root', null, {
+  dialect: 'mysql'
+});
+
+const AuthorModel = db.define('author', {
+  firstName: { type: Sequelize.STRING },
+  lastName: { type: Sequelize.STRING },
+});
+
+const PostModel = db.define('post', {
+  title: { type: Sequelize.STRING },
+  text: { type: Sequelize.STRING }
+});
+
+AuthorModel.hasMany(PostModel);
+PostModel.belongsTo(AuthorModel);
+
+casual.seed(123);
+db.sync({ force: true }).then(() => {
+  _.times(10, () => {
+    return AuthorModel.create({
+      firstName: casual.first_name,
+      lastName: casual.last_name,
+    }).then((author) => {
+      return author.createPost({
+        title: `A post by ${author.firstName}`,
+        text: casual.sentences(3),
+      });
+    });
+  });
+});
+
+const FortuneCookie = {
+  getOne() {
+    return fetch('http://fortunecookieapi.herokuapp.com/v1/cookie')
+      .then(res => res.json())
+      .then(res => {
+        return res[0].fortune.message;
+      });
+  }
+};
+
+const Author = db.models.author;
+const Post = db.models.post;
+
+export { Author, Post, FortuneCookie };
--- data/schema.js
+++ data/schema.js
@@ -1,14 +1,32 @@
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import mocks from './mocks';
+// import mocks from './mocks';
+import resolvers from './resolvers';
 
 const typeDefs = `
 type Query {
-  testString: String
+  author(firstName: String, lastName: String): Author
+  allAuthors: [Author]
+  getFortuneCookie: String # we'll use this later
+}
+
+type Author {
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author
 }
 `;
 
-const schema = makeExecutableSchema({ typeDefs });
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-addMockFunctionsToSchema({ schema, mocks });
+// addMockFunctionsToSchema({ schema, mocks });
 
 export default schema;
